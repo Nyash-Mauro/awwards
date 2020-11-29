7f52c7d2ea5ad3503727fa01171192c98d781f35
Nyash-Mauro <mauricee423@gmail.com>
2020-11-29 15:07:48 +0300

    added updateprofile form

--- .history/award/forms_20201129150554.py
+++ .history/award/forms_20201129150554.py
@@ -1,13 +1,10 @@
 from django import forms
-from . models import *
-
+from . models import  *
 
 class ProjectUpload(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'image', 'description', 'link')
-
-
+        fields = ('title','image','description','link')
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
