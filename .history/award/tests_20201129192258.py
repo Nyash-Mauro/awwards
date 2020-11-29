0c9bd6d724aa1724a0c499cbe1ba9782ee984372
Nyash-Mauro <mauricee423@gmail.com>
2020-11-29 19:33:05 +0300

    added some tests

--- .history/award/tests_20201129192258.py
+++ .history/award/tests_20201129192258.py
@@ -8,7 +8,7 @@ class PostTestClass(TestCase):
     '''
     
     def setUp(self):
-        self.post = Post(id=1,title='new post',image='images/',description='the best of the best', link='https://link.com/',user_id=3)
+        self.post = Post(id=1,title='new post',image='images/lagoon.jpeg',description='the best of the best', link='https://link.com/',user_id=3)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.post,Post))
