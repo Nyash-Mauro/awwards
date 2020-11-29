d16d65d70fa7b4df36162b50bb0ee2f8dbbb9f36
Nyash-Mauro <mauricee423@gmail.com>
2020-11-29 16:08:18 +0300

    added profileserilalizer

--- .history/award/serializers_20201129160732.py
+++ .history/award/serializers_20201129160732.py
@@ -1,14 +1,12 @@
-from .models import Post, Profile
+from .models import Post,Profile
 from rest_framework import serializers
-
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = ('title', 'image', 'description', 'link')
-
+        fields = ('title','image','description','link')
 
 class ProfileSeralizer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Profile
-        fields = (' First_Name', 'Last_Name', ' Email', ' bio', ' profile_pic')
+        fields = (' First_Name','Last_Name',' Email',' bio',' profile_pic')
